b529f2b6d3cc2b1960efa02959eb3aa21687d70e
Starheart <2505909854@qq.com>
2026-03-23 17:32:31 +0800

    feat-logrus初始化

--- core/init_conf.go
+++ core/init_conf.go
@@ -1,32 +1,24 @@
 package core
 
 import (
+	"blogx_server/conf"
+	"blogx_server/flags"
 	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
-var confPath = "settings.yaml"
-
-type System struct {
-	Ip   string `yaml:"ip"`
-	Port int    `yaml:"port"`
-}
-
-type Config struct {
-	System System `yaml:"system"`
-}
-
-func ReadConf() {
-	byteData, err := os.ReadFile(confPath)
+func ReadConf() (c *conf.Config) {
+	byteData, err := os.ReadFile(flags.FlagOptions.File)
 	if err != nil {
 		panic(err)
 	}
-	var config Config
-	err = yaml.Unmarshal(byteData, &config)
+	c = new(conf.Config)
+	err = yaml.Unmarshal(byteData, &c)
 	if err != nil {
 		panic(fmt.Sprintf("yaml配置文件格式错误 %s", err))
 	}
-	fmt.Println(config)
+	fmt.Printf("读取配置文件 %s 成功\n", flags.FlagOptions.File)
+	return
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module blogx_server
 
 go 1.26
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/sirupsen/logrus v1.9.4 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
